c8785284a8a7bda6b77e8898be6ac92a29658ce6
johns <johnschlee@gmail.com>
2023-07-09 16:55:43 -0400

    Add tests for auth button

--- todo-web/setup-jest.ts
+++ todo-web/setup-jest.ts
@@ -1,1 +1,22 @@
 import 'jest-preset-angular/setup-jest';
+
+import { ngMocks } from 'ng-mocks';
+
+// auto spy
+ngMocks.autoSpy('jest');
+
+// In case, if you use @angular/router and Angular 14+.
+// You might want to set a mock of DefaultTitleStrategy as TitleStrategy.
+// A14 fix: making DefaultTitleStrategy to be a default mock for TitleStrategy
+import { DefaultTitleStrategy, TitleStrategy } from '@angular/router';
+import { MockService } from 'ng-mocks';
+ngMocks.defaultMock(TitleStrategy, () => MockService(DefaultTitleStrategy));
+
+// Usually, *ngIf and other declarations from CommonModule aren't expected to be mocked.
+// The code below keeps them.
+import { CommonModule } from '@angular/common';
+import { ApplicationModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+ngMocks.globalKeep(ApplicationModule, true);
+ngMocks.globalKeep(CommonModule, true);
+ngMocks.globalKeep(BrowserModule, true);
--- todo-web/src/app/components/auth-button/auth-button.component.spec.ts
+++ todo-web/src/app/components/auth-button/auth-button.component.spec.ts
@@ -1,12 +1,45 @@
-import { MockBuilder, MockRender } from 'ng-mocks';
+import { MockBuilder, MockRender, ngMocks } from 'ng-mocks';
 import { AuthButtonComponent } from './auth-button.component';
 import { AppModule } from '../../app.module';
+import { of } from 'rxjs';
+import { AuthService } from '@auth0/auth0-angular';
+import * as exp from 'constants';
 
 describe('AuthButtonComponent', () => {
-  beforeEach(() => MockBuilder(AuthButtonComponent, [AppModule]));
+  beforeEach(() => {
+    return MockBuilder(AuthButtonComponent, [AppModule]).mock(AuthService, {
+      isAuthenticated$: of(false),
+    });
+  });
 
   it('should create', () => {
     const fixture = MockRender(AuthButtonComponent);
     expect(fixture.point.componentInstance).toBeTruthy();
+  });
+
+  it('it should invoke auth.loginWithRedirect() when the Log in button is clicked', () => {
+    const fixture = MockRender(AuthButtonComponent);
+    const button = fixture.debugElement.nativeElement.querySelector('button');
+    button.click();
+    expect(
+      fixture.point.componentInstance.auth.loginWithRedirect,
+    ).toHaveBeenCalled();
+  });
+
+  it('it should invoke auth.logout() when the Log out button is clicked', async () => {
+    const fixture = MockRender(AuthButtonComponent);
+    ngMocks.stubMember(
+      fixture.point.componentInstance.auth,
+      'isAuthenticated$',
+      of(true),
+    );
+    fixture.detectChanges();
+
+    const button = fixture.debugElement.nativeElement.querySelector('button');
+    expect(button).toBeTruthy();
+    button.click();
+    fixture.whenStable().then(() => {
+      expect(fixture.point.componentInstance.auth.logout).toHaveBeenCalled();
+    });
   });
 });
