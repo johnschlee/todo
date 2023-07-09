84ba67bc51d3168514312db6d55def05da8d061d
johns <johnschlee@gmail.com>
2023-07-09 17:11:21 -0400

    Fix out put file

--- todo-web/jest.config.js
+++ todo-web/jest.config.js
@@ -2,4 +2,5 @@ module.exports = {
   preset: "jest-preset-angular",
   setupFilesAfterEnv: ["<rootDir>/setup-jest.ts"],
   globalSetup: "jest-preset-angular/global-setup",
+  coverageReporters: ['cobertura'],
 };
