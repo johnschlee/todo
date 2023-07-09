95112fc1b934a54591837a654ad4ab6d36e71999
johns <johnschlee@gmail.com>
2023-07-09 14:06:11 -0400

    Update build to call config script

--- todo-web/src/environments/set-env.ts
+++ todo-web/src/environments/set-env.ts
@@ -3,27 +3,35 @@
 const setEnv = () => {
   const fs = require('fs');
   const writeFile = fs.writeFile;
-// Configure Angular `environment.ts` file path
-  const targetPath = './src/environments/environment.production.ts';
-// Load node modules
+  // Configure Angular `environment.ts` file path
+  const targetPath = './src/environments/environment.ts';
+  // Load node modules
   const colors = require('colors');
   require('dotenv').config({
-    path: 'src/environments/.env'
+    path: 'src/environments/.env',
   });
-// `environment.ts` file structure
+  // `environment.ts` file structure
   const envConfigFile = `export const environment = {
-  auth0_domain: '${process.env["AUTH0_DOMAIN"]}',
-  auth0_clientId: '${process.env["AUTH0_CLIENT_ID"]}',
+  auth0_domain: '${process.env['AUTH0_DOMAIN']}',
+  auth0_clientId: '${process.env['AUTH0_CLIENT_ID']}',
   production: true,
 };
 `;
-  console.log(colors.magenta('The file `environment.ts` will be written with the following content: \n'));
-  writeFile(targetPath, envConfigFile, (err:any) => {
+  console.log(
+    colors.magenta(
+      'The file `environment.ts` will be written with the following content: \n'
+    )
+  );
+  writeFile(targetPath, envConfigFile, (err: any) => {
     if (err) {
       console.error(err);
       throw err;
     } else {
-      console.log(colors.magenta(`Angular environment.ts file generated correctly at ${targetPath} \n`));
+      console.log(
+        colors.magenta(
+          `Angular environment.ts file generated correctly at ${targetPath} \n`
+        )
+      );
     }
   });
 };
